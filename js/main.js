b9394b70485014f912cc2cb68a3088a671b6a8d8
Joesh Gichon <joeshgichon56@gmail.com>
2021-08-05 20:35:28 +0300

    This will add the order section, style it appropriately and create a js functionality that collects order, calculate price and displays order details at the bottom

--- js/main.js
+++ js/main.js
@@ -1,179 +1,87 @@
-// --------------------------User interface-------------------
-function MenuItem(pizzaName,price,quantity){
-    this.pizzaName=pizzaName;
-    this.price=price;
-    this.quantity=quantity
-}
-var cheesePizza = new MenuItem(
-    $(document).ready(function(){
-        pizzaName = "cheese pizza";
-        price = 1000;
-        quantity = $("#cheese-value").val();
-        return console.log(`This ${pizzaName} is worth ${price}`);
-    })
-);
-var veggiePizza = new MenuItem(
-    $(document).ready(function(){
-        pizzaName = "veggie-Pizza";
-        price = 1200;
-        return console.log(`This ${pizzaName} is worth ${price}`);
-    })
-);
-var chickenPizza = new MenuItem(
-    $(document).ready(function(){
-        pizzaName = "chicken-pizza";
-        price = 1400;
-        return console.log(`This ${pizzaName} is worth ${price}`);
-    })
-);
-var pepperoniPizza = new MenuItem(
-    $(document).ready(function(){
-        pizzaName = "pepperoni-pizza";
-        price = 1100;
-        return console.log(`This ${pizzaName} is worth ${price}`);
-    })
-);
-var meatPizza = new MenuItem(
-    $(document).ready(function(){
-        pizzaName = "meat-pizza";
-        price = 1500;
-        return console.log(`This ${pizzaName} is worth ${price}`);
-    })
-);
-
-var margheritaPizza = new MenuItem(
-    $(document).ready(function(){
-        pizzaName = "margherita-pizza";
-        price = 2000;
-        return console.log(`This ${pizzaName} is worth ${price}`);
-    })
-);
-
-
-// ------------------------------End---------------------------
-
-
 // backend logic/business logic
-// forms data collection
+// Order data collection
 
+function Order (size,crust,topping,number,acquisition){
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.number = number;
+    this.acquisition = acquisition;
+}
+  
+var sizePrice = {
+    small : 600,
+    medium : 800,
+    large : 1000,
+    extraLarge : 1400,
+};
+  
+var crustPrice = {
+    cheeseStuffed : 200,
+    flatBread : 200,
+    thin : 100,
+    glutenFree : 340,
+};
+  
+var toppingPrice = {
+    chickenHawaiian : 200,
+    cheeseBurger : 300,
+    pepperoni : 400,
+    veggie : 500,
+};
+
+let totalPrice = function(size,crust,topping,number) {
+    return (size + crust + topping)*number;
+}
+
+String.prototype.addSign = function () {
+    return "Ksh." + this;
+}
+
+//user interface
 $(document).ready(function(){
-    $("form#collect").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
+$("form#ordered").submit(function(event){
+    event.preventDefault();
 
-        // acquiring inputed values
-        let firstName = $("#mce-FNAME").val();
-        let lastName = $("#mce-LNAME").val();
-        let email = $("#mce-EMAIL").val();
-        let comments = $("#comments").val();
-        
-        alert("Thank You" + " " + firstName + " " + lastName + " " + "for reaching you us.")
-    });
-    (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';}(jQuery));var $mcj = jQuery.noConflict(true)
+    var pizzaSize = $("#pizza-size").val();
+    var pizzaCrust = $("#pizza-crust").val();
+    var pizzaTopping = $("#pizza-topping").val();
+    var numberOrdered = parseInt($("#number-of-pizza").val());
+    var acquisitionMethod = $("#acquisition-method").val();
+
+    var orderMade = new Order (pizzaSize,pizzaCrust,pizzaTopping,numberOrdered,acquisitionMethod);
+
+    var price = totalPrice(sizePrice[orderMade.size],crustPrice[orderMade.crust],toppingPrice[orderMade.topping],orderMade.number);
+
+    $("#total-charge").text(price.toString().addSign());
+
+    $("#output").append("<tr>" + "<td>" + orderMade.size + "</td>" + 
+                        "<td>" + orderMade.crust + "</td>" +  
+                        "<td>" + orderMade.topping + "</td>" +  
+                        "<td>" + orderMade.number + "</td>" + 
+                        "</tr>");
+
+    resetFields();
+    
+    if (orderMade.acquisition === "delivered") {
+    var location = prompt("Input delivery location.ie estate/street,County");
+    alert ("Your order will be delivered to your location.");
+    } else {
+    return false;
+    }
+
+    
+    resetFields();
+
 });
 
-// menu items form data collection
+function resetFields () {
+    $("#pizza-size").val("");
+    $("#pizza-crust").val("");
+    $("#pizza-topping").val("");
+    $("#number-of-pizza").val("");
+    $("#acquisition-method").val("");
+}
 
-
-$(document).ready(function(){
-    $("form#cheese-form").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
-
-        var qnt = $("#cheese-value").val()
-        console.log(qnt);
-        var prc = $("#cheese-price").change(function(){
-            var selectedCheesePizza = $("#cheese-price option:selected")
-            var cheesePrice = selectedCheesePizza.val();
-            return cheesePrice;
-        });
-        console.log(prc.val());
-        alert(`You have ordered Cheese Pizza worth ${prc.val()*qnt}`);
-    });
 });
 
-$(document).ready(function(){
-    $("form#Veggie-form").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
-
-        var qnt = $("#Veggie-value").val()
-        console.log(qnt);
-        var prc = $("#Veggie-price").change(function(){
-            var selectedVeggiePizza = $("#Veggie-price option:selected")
-            var veggiePrice = selectedCheesePizza.val();
-            return veggiePrice;
-        });
-        console.log(prc.val());
-        alert(`You have ordered Veggie Pizza worth ${prc.val()*qnt}`);
-    });
-});
-
-$(document).ready(function(){
-    $("form#Chicken-form").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
-
-        var qnt = $("#Chicken-value").val()
-        console.log(qnt);
-        var prc = $("#Chicken-price").change(function(){
-            var selectedChickenPizza = $("#Chicken-price option:selected")
-            var chickenPrice = selectedChickenPizza.val();
-            return chickenPrice;
-        });
-        console.log(prc.val());
-        alert(`You have ordered Chicken Pizza worth ${prc.val()*qnt}`);
-    });
-});
-
-$(document).ready(function(){
-    $("form#Pepperoni-form").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
-
-        var qnt = $("#Pepperoni-value").val()
-        console.log(qnt);
-        var prc = $("#Pepperoni-price").change(function(){
-            var selectedPepperoniPizza = $("#Pepperoni-price option:selected")
-            var pepperoniPrice = selectedPepperoni.val();
-            return pepperoniPrice;
-        });
-        console.log(prc.val());
-        alert(`You have ordered Pepperoni Pizza worth ${prc.val()*qnt}`);
-    });
-});
-
-$(document).ready(function(){
-    $("form#Meat-form").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
-
-        var qnt = $("#Meat-value").val()
-        console.log(qnt);
-        var prc = $("#Meat-price").change(function(){
-            var selectedMeatPizza = $("#Meat-price option:selected")
-            var meatPrice = selectedMeatPizza.val();
-            return meatPrice;
-        });
-        console.log(prc.val());
-        alert(`You have ordered Meat Pizza worth ${prc.val()*qnt}`);
-    });
-});
-
-$(document).ready(function(){
-    $("form#Margherita-form").submit(function(event){
-        // preventing default form submission to itself
-        event.preventDefault();
-
-        var qnt = $("#Margherita-value").val()
-        console.log(qnt);
-        var prc = $("#Margherita-price").change(function(){
-            var selectedMargheritaPizza = $("#Margherita-price option:selected")
-            var margheritaPrice = selectedMargheritaPizza.val();
-            return margheritaPrice;
-        });
-        console.log(prc.val());
-        alert(`You have ordered Margherita Pizza worth ${prc.val()*qnt}`);
-    });
-});
-// -------------------End-----------------------------------
